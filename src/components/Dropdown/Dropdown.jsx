b667672623729bc40a0c1f1d1b1283a291fe6e07
Vinicios Neves <vinny@macbook.lan>
2023-02-04 16:00:25 +0000

    adicionando estilos no button da dropdown

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,20 +1,57 @@
 import { useState } from "react";
+import styled from '@emotion/styled'
 
-export const Dropdown = ({label, placeholder}) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleDropdownButtonClick = () => {
-    setIsOpen(!isOpen);
-  };
-  return (
-        <label>
+const StyleLabel = styled.label({
+    display: 'block',
+    textAlign: 'left'
+})
+
+const StyledButton = styled.button`
+    cursor: pointer;
+    display: block;
+    height: 40px;
+    width: 100%;
+    font-size: 18px;
+    outline: none;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-weight: 400;
+    border-radius: 18px;
+    border-bottom-left-radius: ${props => props.isOpen ? '0' : '18px'};
+    border-bottom-right-radius: ${props => props.isOpen ? '0' : '18px'};
+    margin-top: ${props => props.theme.spacing.xs};
+    margin-bottom: ${props => props.theme.spacing.s};
+    padding: ${props => props.theme.spacing.s};
+    background: ${props => props.theme.colors.white};
+    border: 1px solid ${props => props.theme.colors.neutral.b};
+`
+const StyledPlaceholder = styled.span`
+    color: ${props => props.theme.colors.neutral.b};
+`
+export const Dropdown = ({ label, placeholder }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleDropdownButtonClick = () => {
+        setIsOpen(!isOpen);
+    };
+    return (
+        <StyleLabel>
             {label}
-            <button
+            <StyledButton
+                isOpen={isOpen  }
                 type="button"
                 onClick={handleDropdownButtonClick}
             >
-                {placeholder}
-                <span>{isOpen ? '▲' : '▼'}</span>
-            </button>
-        </label>
+                <div>
+                    <StyledPlaceholder>
+                        {placeholder}
+                    </StyledPlaceholder>
+                </div>
+                <div>
+                    <span>{isOpen ? '▲' : '▼'}</span>
+                </div>
+            </StyledButton>
+        </StyleLabel>
     )
 }
